d233ba231279ac2789082d8ee6419d55196eece6
Chris Martin <martin.chris.30@live.com>
2023-05-16 00:31:23 +0100

    Trying to containerize

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%@+r)jd!p)acau*ua6zi0q2i76vs#vo5)xwj@%y#d75ujz3+0*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user_auth',
     'party',
 ]
